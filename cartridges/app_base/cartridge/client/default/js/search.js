98569e43c1fc1dd97c1c298ce5da3f1f89291e2d
andrii-telishko <telishkoav@gmail.com>
2021-11-25 16:17:16 +0200

    commit

--- cartridges/app_base/cartridge/client/default/js/search.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var processInclude = require("./util");
-
-$(document).ready(function () {
-    processInclude(require("./search/search"));
-    processInclude(require("./product/quickView"));
-    processInclude(require("./product/wishlistHeart"));
-});
